712b9d60c01a036b235238a93e9f5abc5a4f3e8d
alottabeans <50392507+alottabeans@users.noreply.github.com>
2020-11-11 23:55:04 -0800

    Add files via upload

---
+++ instructions.h
@@ -0,0 +1,14 @@
+#ifndef INSTR_H
+#define INSTR_H
+
+
+//INSTRUCTION SET
+#define NOP(pc) (pc + 1) //increment pc
+#define LD_bc_u16(reg, val) ((val = reg))  
+#define ADD()
+#define SUB()
+#define OR()
+#define INC()
+
+#endif
+

1cc37291a34346dfae52d292d84a30d0f1e6a9de
alottabeans <50392507+alottabeans@users.noreply.github.com>
2020-12-02 13:07:11 -0800

    Add files via upload

--- src/opcodes.c
+++ src/opcodes.c
@@ -1,6 +1,15 @@
 #include "opcodes.h"
 #include <stdio.h>
 
+U16 combine_regs(U8 r1, U8 r2)
+{
+  U16 ret;
+
+  ret = r1 << 4 | r2;
+
+  return ret;
+}
+
 void ld_r16_n16(U16 r, U16 n)
 {
   r = n;
@@ -31,4 +40,9 @@ void ldd_r16_n8(U16 r, U8 n)
 void inc_regs(U16 regs)
 {
   regs++;
+}
+
+void dec_regs(U16 regs)
+{
+  regs--;
 }
--- src/opcodes.h
+++ src/opcodes.h
@@ -7,8 +7,9 @@
 #define UPPER(x) (x >> 4)
 #define LOWER(x) (x & 0x0F)
 
-#define COMB_REGS(r1, r2) (r1 << 4 | r2)
 #define NOP(pc) (pc++)
+
+U16 combine_regs(U8 r1, U8 r2);
 
 void ld_r16_n16(U16 r, U16 n);
 void ld_r16_n8(U16 r,  U8 n);
@@ -17,6 +18,7 @@ void ldi_r16_n8(U16 r, U8 n);
 void ldd_r16_n8(U16 r, U8 n); 
 
 void inc_regs(U16 reg);
+void dec_regs(U16 reg);
 
 #endif
 
